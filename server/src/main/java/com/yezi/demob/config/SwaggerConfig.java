73ebc76152e0b109439c1485d18a343237aeeefe
YeJingjing <yjjjng0901@163.com>
2019-04-18 13:07:48 +0800

    add swagger config

---
+++ server/src/main/java/com/yezi/demob/config/SwaggerConfig.java
@@ -0,0 +1,28 @@
+package com.yezi.demob.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * @Author: yezi
+ * @Date: 2019/4/18 11 27
+ * @Description: swagger 配置
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+}
